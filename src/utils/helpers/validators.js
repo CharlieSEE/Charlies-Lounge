7ad32d1ff13a32035a8c33c6046ee42666e2922b
CharlieSEE <kmadej148d@gmail.com>
2022-03-24 13:11:43 +0100

    Feat: Password validator

--- src/utils/helpers/validators.js
+++ src/utils/helpers/validators.js
@@ -1,8 +1,24 @@
+//@ts-check
 /* eslint-disable no-useless-escape */
-const emailValidator = (email) => {
+
+/**
+ * Function validating email with General Email Regex (RFC 5322 Official Standard)
+ * @param {String} email Email to be validated
+ * @returns true if email is valid
+ */
+const validateEmail = (email) => {
   const testRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
   return testRegex.test(email);
 };
 
-export { emailValidator };
+/**
+ * Function checking if password is at least 6 characters long
+ * @param {String} password String containing password
+ * @returns true if password is at least 6 characters long
+ */
+const validatePassword = (password) => {
+  return password.length > 6;
+};
+
+export { validateEmail, validatePassword };
