e31dc834089d8df1db8622e7119c7ba1bbc75974
CharlieSEE <kmadej148d@gmail.com>
2022-03-24 13:47:41 +0100

    Feat: Form error validation

--- src/components/Pages/SignUpPage/SignUpPage.jsx
+++ src/components/Pages/SignUpPage/SignUpPage.jsx
@@ -2,6 +2,12 @@ import { useState, useContext } from "react";
 import { SupabaseContext } from "../../Context/SupabaseContext";
 import { useNavigate } from "react-router-dom";
 
+import {
+  validateEmail,
+  validatePassword,
+  validateUsername,
+} from "../../../utils/helpers/validators";
+
 import TextField from "../../Shared/Form/TextField/TextField";
 import Submit from "../../Shared/Form/Submit/Submit";
 
@@ -9,20 +15,52 @@ import styles from "./SignUpPage.module.css";
 
 const SignUpPage = () => {
   const [userName, setUserName] = useState("");
+  const [userNameError, setUserNameError] = useState(false);
   const [userEmail, setUserEmail] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] =
+    useState("Type proper email");
   const [userPassword, setUserPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
   const [userPasswordConfirmation, setUserPasswordConfirmation] = useState("");
+  const [passwordConfirmationError, setPasswordConfirmationError] =
+    useState(false);
 
   const supabase = useContext(SupabaseContext);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (userPassword === userPasswordConfirmation) {
-      console.log("ZŁE HASŁO");
+
+    if (!validateUsername(userName)) {
+      setUserNameError(true);
       return;
+    } else {
+      setUserNameError(false);
     }
-    const { user, session, error } = await supabase.auth.signUp(
+
+    if (!validateEmail(userEmail)) {
+      setEmailError(true);
+      return;
+    } else {
+      setEmailError(false);
+    }
+
+    if (!validatePassword(userPassword)) {
+      setPasswordError(true);
+      return;
+    } else {
+      setPasswordError(false);
+    }
+
+    if (userPassword !== userPasswordConfirmation) {
+      setPasswordConfirmationError(true);
+      return;
+    } else {
+      setPasswordConfirmationError(false);
+    }
+
+    const { user, error } = await supabase.auth.signUp(
       {
         email: userEmail,
         password: userPassword,
@@ -34,8 +72,23 @@ const SignUpPage = () => {
       }
     );
     if (user) navigate("/dashboard");
-    if (error) console.error(error);
-    if (session) console.log(session);
+    if (error) {
+      switch (error.status) {
+        case 400:
+          setEmailError(true);
+          setEmailErrorMessage(error.message);
+          setPasswordError(false);
+          setUserName(false);
+          setPasswordConfirmationError(false);
+          break;
+
+        default:
+          setEmailError(true);
+          setPasswordError(true);
+          setUserName(true);
+          break;
+      }
+    }
   };
   return (
     <div className={styles.container}>
@@ -46,6 +99,9 @@ const SignUpPage = () => {
           placeholder="Username"
           type="text"
           label="Username"
+          error={userNameError}
+          errorMessage="Username must be at least 4 characters long"
+          maxLength={30}
           htmlFor="signup-username"
           value={userName}
           onChange={(e) => {
@@ -55,6 +111,8 @@ const SignUpPage = () => {
         <TextField
           id="signup-email"
           type="email"
+          error={emailError}
+          errorMessage={emailErrorMessage}
           placeholder="Email"
           label="Email"
           htmlFor="signup-email"
@@ -67,6 +125,8 @@ const SignUpPage = () => {
           id="signup-password"
           type="password"
           placeholder="Password"
+          error={passwordError}
+          errorMessage="Password must be at least 6 characters long"
           label="Password"
           htmlFor="signup-password"
           onChange={(e) => {
@@ -79,6 +139,8 @@ const SignUpPage = () => {
           type="password"
           placeholder="Retype password"
           label="Retype password"
+          error={passwordConfirmationError}
+          errorMessage="Passwords don't match"
           htmlFor="signup-password-confirm"
           onChange={(e) => {
             setUserPasswordConfirmation(e.target.value);
